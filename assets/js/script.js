c5dfbfb9947dc590fd0cb7a01dde496c6ca3b41c
Rafael S. de Lima <rafael.lima@startse.com>
2023-02-17 21:41:09 -0300

    Funcionalidade de aparecer o botão de comprar ao usuário passar o mouse sobre o card

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function mostraBotao(idBotao) {
+    let botao = document.getElementById(idBotao)
+
+    botao.style.display = 'block'
+}
+
+function ocultarBotao(idBorao) {
+    let botao = document.getElementById(idBorao)
+
+    botao.style.display = 'none'
+}
